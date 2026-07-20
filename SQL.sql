e9f0639d83781b6211da6f2e43a0bd1de833e0f4
Qasim Mirza <152003008+qasimmirza96@users.noreply.github.com>
2026-07-20 22:46:03 +0500

    Update SQL database setup guide
    Added a note about testing to the SQL database setup guide.

--- SQL.sql
+++ SQL.sql
@@ -3,3 +3,5 @@ Creating an Azure SQL Database involves several steps, all of which can be done 
 
 1. In the Azure Portal, click the “Create a resource” button in the homepage's upper left corner. In the new resource page, type “SQL Database’ in the search box and select it from the list of services.
   CQRS is a design pattern in software architecture
+
+  testing
